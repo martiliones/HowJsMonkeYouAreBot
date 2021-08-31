6a5cb3a00025d4c5170adaf73a6e8cfa9b864b0b
martiliones <martiliones@yandex.com>
2021-08-31 16:08:07 +0600

    Add switch language button

--- src/menu.js
+++ src/menu.js
@@ -1,12 +1,28 @@
 const { InlineMenu, i18n, siq, link } = require('@libtelegram/helpers');
 
+const switchLanguage = async (usr, cb) => {
+  // get current language
+  const lang = usr.locale();
+
+  // set locale
+  usr.locale(
+    lang === 'ru'?'en':'ru'
+  );
+
+  // rerender menu
+  usr.reenter(cb);
+
+  await cb.answer(usr.__('language.success'));
+};
+
 const menu = InlineMenu({
   Id: 'menu',
   Text: 'menu.text',
   wrapper: [i18n],
 }, [
   [['menu.button', siq()]],
-  [['menu.link', link('https://github.com/martiliones/HowJsMonkeYouAreBot')]]
+  [['menu.link', link('https://github.com/martiliones/HowJsMonkeYouAreBot')]],
+  [['menu.switch_language', switchLanguage]]
 ]);
 
 module.exports = menu;
